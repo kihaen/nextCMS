9d06fe75a4031d2fc0b8363593d967de5adea4d2
kihaen <kihaen2@gmail.com>
2024-08-26 16:38:28 -0400

    chore: fix server

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  url: env('STRAPI_URL'),
+  }
 });
